aacaed3d5cd853025f9c6296028604ba6fd148b2
bibhor dhungel <bd@senya.app>
2025-10-23 00:46:38 -0400

    add tags

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -64,6 +64,7 @@ export default async function AdminDashboard() {
         project_id,
         object_key,
         notes,
+        tags,
         upload_status,
         status,
         projects ( name )
